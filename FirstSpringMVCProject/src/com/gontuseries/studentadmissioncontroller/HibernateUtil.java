7121ca143116c24e6d64907e102b6db4239e0e25
Yang Yang <yaya7263@colorado.edu>
2018-05-01 02:41:24 -0600

    Update HibernateUtil.java

--- FirstSpringMVCProject/src/com/gontuseries/studentadmissioncontroller/HibernateUtil.java
+++ FirstSpringMVCProject/src/com/gontuseries/studentadmissioncontroller/HibernateUtil.java
@@ -4,7 +4,7 @@ import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
-
+// partly from gontu.org
 public class HibernateUtil {
 
 	private static final SessionFactory sessionFactory = buildSessionFactory();
